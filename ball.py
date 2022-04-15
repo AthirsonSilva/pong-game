3769096ac8a6ba6e1f40de6d164f4e9af2a822c3
Athirson <Athirsonarceus@gmail.com>
2022-04-15 14:04:46 -0300

    Ball drawing

--- ball.py
+++ ball.py
@@ -3,6 +3,11 @@ from turtle import Turtle
 
 class Ball(Turtle):
     """ Creating the ball """
-    def __init__():
-        super()__init__()
-        pass
+
+    def __init__(self):
+        super().__init__()
+        self.shape('circle')
+        self.penup()
+        self.color('yellow')
+        self.speed('fast')
+        self.goto(0, 0)
