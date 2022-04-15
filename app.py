a75961a79b5d1230fca44bc4f1e613d7f20cbb98
Athirson <Athirsonarceus@gmail.com>
2022-04-15 14:05:01 -0300

    Game start screen

--- app.py
+++ app.py
@@ -1,8 +1,73 @@
+from ball import Ball
+from paddle import Paddle
+from scoreboard import Scoreboard
+from time import sleep
 from turtle import Screen
-from turtle import Turtle
 
+
+# Creating the screen
 screen = Screen()
 screen.bgcolor('black')
 screen.title('Pong')
+screen.setup(height=600, width=600)
 
+
+def draw_dashed():
+    """Drawing dashed lines challenge
+        """
+    from turtle import Turtle
+
+    turtle = Turtle(shape='square')
+    turtle.color('white')
+    turtle.penup()
+    turtle.shapesize(stretch_wid=0.75, stretch_len=0.33)
+    turtle.setposition(x=0, y=315)
+    turtle.setheading(270)
+    turtle.shapesize(stretch_wid=0.33, stretch_len=0.75)
+
+    for i in range(1, 32):
+        turtle.fd(10)
+        turtle.penup()
+        turtle.fd(10)
+        turtle.pendown()
+
+
+draw_dashed()
+
+
+# Creating paddles
+paddle1 = Paddle()
+paddle1.create_paddle('blue', 250, 0)
+paddle2 = Paddle()
+paddle2.create_paddle('red', -250, 0)
+
+# Creating the ball
+ball = Ball()
+
+# Creating scoreboard
+scoreboard_p1 = Scoreboard()
+scoreboard_p2 = Scoreboard()
+
+# Key strokes
+screen.listen()
+
+# Paddle 1 movements
+screen.onkey(paddle1.up, 'w')
+screen.onkey(paddle1.down, 's')
+
+# Paddle 2 movements
+screen.onkey(paddle2.up, 'Up')
+screen.onkey(paddle2.down, 'Down')
+
+# Game screen loop
+game_is_on = True
+while game_is_on:
+    screen.update()
+    scoreboard_p1.write_score(arg=scoreboard_p1.score,
+                              align='center', x=-50, y=270)
+    scoreboard_p2.write_score(arg=scoreboard_p2.score,
+                              align='center', x=50, y=270)
+    sleep(0.1)
+
+# Keeping the screen up till user click
 screen.exitonclick()
