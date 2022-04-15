1fc07a79fcb0f55de4ae1b7cc1599f8f3c4ecfc0
Athirson <Athirsonarceus@gmail.com>
2022-04-15 14:02:52 -0300

    Scoreboard drawing

--- scoreboard.py
+++ scoreboard.py
@@ -2,7 +2,17 @@ from turtle import Turtle
 
 
 class Scoreboard(Turtle):
-	""" Scoreboard printing and manipulation """
-    def __init__():
+    """ Scoreboard printing and manipulation """
+
+    def __init__(self):
         super().__init__()
-        pass
+        self.score = 0
+        self.penup()
+        self.hideturtle()
+
+    def write_score(self, arg, align, x, y):
+        """ Write the score of the player 1 """
+
+        self.write(arg=arg, align=align, font='Arial')
+        self.color('white')
+        self.goto(x, y)
