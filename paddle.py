1589a0c5c91567bf206e6d4e212e1795bb915060
Athirson <Athirsonarceus@gmail.com>
2022-04-15 14:03:09 -0300

    Two paddles setup

--- paddle.py
+++ paddle.py
@@ -1,8 +1,27 @@
 from turtle import Turtle
 
 
-class Paddle:
-	""" Paddle model class """
-    def __init__():
+class Paddle(Turtle):
+    """ Paddle model class """
+
+    def __init__(self):
         super().__init__()
-        pass
+
+    def create_paddle(self, color, x, y):
+        """ Creating the two paddles """
+        self.shape('square')
+        self.penup()
+        self.color(color)
+        self.shapesize(stretch_wid=5, stretch_len=0.75)
+        self.speed('fast')
+        self.goto(x=x, y=y)
+
+    def up(self):
+        """ Move the paddle up """
+        new_y = self.ycor() + 20
+        self.goto(x=self.xcor(), y=new_y)
+
+    def down(self):
+        """ Move the paddle down """
+        new_y = self.ycor() - 20
+        self.goto(x=self.xcor(), y=new_y)
